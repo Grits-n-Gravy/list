d1af2a12b199892d3a2d1793edc1fabf8dfac9b1
christian <christianjones94@gmail.com>
2022-09-30 17:40:17 -0400

    further refactoting

--- main.js
+++ main.js
@@ -11,12 +11,33 @@ document.addEventListener("DOMContentLoaded", (e) => {
 let addToDoButton = document.getElementById("addToDo");
 let addToDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("input_field");
+let addButton = document.createElement("button");
 
 // function for appending data into "To Do List"
 function createTask() {
   let paragraph = document.createElement("p");
   paragraph.innerText = inputField.value;
+  addButton.innerText = "+";
+  paragraph.appendChild(addButton);
   addToDoContainer.appendChild(paragraph);
+  createSubTask();
+}
+
+function createSubTask() {
+  var paragraph = document.createElement("p");
+  var itemInput = document.createElement("input");
+  var itemLi = document.createElement("ol");
+
+  paragraph.appendChild(itemInput);
+  paragraph.appendChild(itemLi);
+  addToDoContainer.appendChild(paragraph);
+
+  addButton.addEventListener("click", function () {
+    var listItem = document.createElement("li");
+    itemLi.appendChild(listItem);
+    listItem.innerText = itemInput.value;
+    itemInput.value = "";
+  });
 }
 
 function removeInput() {
@@ -34,13 +55,13 @@ function strikeThrough() {
 // appends data on "Enter" key
 inputField.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
-    addInput();
+    createTask();
     inputField.value = "";
   }
 });
 
 // appends data on button click
 addToDoButton.addEventListener("click", function () {
-  addInput();
+  createTask();
   inputField.value = "";
 });
